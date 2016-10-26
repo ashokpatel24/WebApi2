9c4073bc115a2daf4e2526bed80725294d94e074
A738567 <A738567@A738567-W10>
2016-10-26 12:18:18 +0530

    Add project files.

---
+++ ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,19 @@
+﻿using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product{Id=, },
+
+        };
+    }
+}
